d3f0248fedfc2e37272fc5ee7ed2f32e121dfb4b
Zuxelator <zuxel87@mail.ru>
2021-05-26 21:29:25 +0400

    5.2.1. Реализовать SimpleArray<T> [#156]

---
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,57 @@
+package ru.job4j.generics;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArray<T> implements Iterable<T> {
+
+    private Object[] array;
+
+    private int capacity = 0;
+    private int cursor = 0;
+
+    public SimpleArray(int size) {
+        this.array =  new Object[size];
+    }
+
+    public void add(T model) {
+        Objects.checkIndex(capacity, array.length);
+        array[capacity++] = model;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, capacity);
+        return (T) array[index];
+    }
+
+    public void set(int index, T model) {
+        Objects.checkIndex(index, capacity);
+        array[index] = model;
+    }
+
+    public void remove(int index) {
+        Objects.checkIndex(index, capacity);
+        System.arraycopy(array, index + 1, array, index, capacity - index - 1);
+        array[capacity - 1] = null;
+        capacity--;
+    }
+
+   @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return capacity > cursor;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) array[cursor++];
+            }
+        };
+    }
+}
