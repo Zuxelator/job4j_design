3213d08546f87dd306422bd074d1527fcd02b060
Zuxelator <zuxel87@mail.ru>
2021-05-30 19:47:22 +0400

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -19,6 +19,45 @@ public class ForwardLinked<T> implements Iterable<T> {
         tail.next = node;
     }
 
+    public void addFirst(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        } else {
+            T rsl = head.value;
+            head = head.next;
+            return rsl;
+        }
+    }
+
+    public T deleteLast() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        if (head.next == null) {
+            T rsl = head.value;
+            head = null;
+            return rsl;
+        }
+        Node<T> node = head;
+        while (node.next.next != null) {
+            node = node.next;
+        }
+        T rsl = node.next.value;
+        node.next = null;
+        return rsl;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
@@ -39,16 +78,6 @@ public class ForwardLinked<T> implements Iterable<T> {
                 return value;
             }
         };
-    }
-
-    public T deleteFirst() {
-        if (head == null) {
-            throw new NoSuchElementException();
-        } else {
-            T rsl = head.value;
-            head = head.next;
-            return rsl;
-        }
     }
 
     private static class Node<T> {
