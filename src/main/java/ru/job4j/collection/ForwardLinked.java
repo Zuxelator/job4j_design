db9ec773446831172fe727c124c0d43513ec7d54
Zuxelator <zuxel87@mail.ru>
2021-06-01 02:08:25 +0400

    6. Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -58,6 +58,23 @@ public class ForwardLinked<T> implements Iterable<T> {
         return rsl;
     }
 
+    public boolean revert() {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Node<T> prev = null;
+        Node<T> current = head;
+        Node<T> next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return true;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
