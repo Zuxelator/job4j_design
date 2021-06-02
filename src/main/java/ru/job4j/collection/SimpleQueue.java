6181326a09a21d54748e3623d4c4209245194774
Zuxelator <zuxel87@mail.ru>
2021-06-02 13:27:52 +0400

    5. Очередь на двух стеках [#160 #162602]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,52 +1,19 @@
 package ru.job4j.collection;
 
 public class SimpleQueue<T> {
-    private SimpleStack<T> in = new SimpleStack<>();
-    private SimpleStack<T> out = new SimpleStack<>();
-    private int inSize;
-    private int outSize;
-
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
-        /*while (inSize != 0) {
-            out.push(in.pop());
-            inSize--;
-            outSize++;
-        }
-        T rsl = out.pop();
-        outSize--;
-        while (outSize != 0) {
-            in.push(out.pop());
-            inSize++;
-            outSize--;
-        }
-        return rsl;*/
-
-        int iterationCount = inSize * 2 - 1;
-        int newInSize = inSize - 1;
-        T rsl = null;
-        SimpleStack<T> temp;
-        while (iterationCount != 0) {
-            out.push(in.pop());
-            inSize--;
-            if (inSize == 0) {
-                rsl = out.pop();
-                temp = in;
-                in = out;
-                out = temp;
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
             }
-            iterationCount--;
         }
-        temp = in;
-        in = out;
-        out = temp;
-        inSize = newInSize;
-        return rsl;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
-        inSize++;
     }
-
 }
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -2,12 +2,19 @@ package ru.job4j.collection;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private int size;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     public void push(T value) {
+        size++;
         linked.addFirst(value);
     }
 }
