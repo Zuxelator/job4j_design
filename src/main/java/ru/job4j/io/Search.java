d418e5f61d04aa8bbc7cf5ffe6dfd736aaf80eab
Zuxelator <zuxel87@mail.ru>
2021-06-16 18:45:38 +0400

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -3,14 +3,22 @@ package ru.job4j.io;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("log")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Should be 2 args. args: " + args.length);
+        }
+        if (!Files.exists(Path.of(args[0]))) {
+            throw new IllegalArgumentException("No such directory. Check args");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("args[1] should starts with \".\" Check args");
+        }
+        Path start = Path.of(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
