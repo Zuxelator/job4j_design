3c5a08f5f848fdb3f93618d8d78701a2c28d2f97
Zuxelator <zuxel87@mail.ru>
2021-06-22 18:14:21 +0400

    6. Кодировка. [#862 #169947]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -2,9 +2,7 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class ConsoleChat {
     private final String path;
@@ -12,21 +10,32 @@ public class ConsoleChat {
     private static final String OUT = "закончить";
     private static final String STOP = "стоп";
     private static final String CONTINUE = "продолжить";
-    private static Map<Integer, String> map = new HashMap<>();
+    private static List<String> list = new ArrayList<>();
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
         this.botAnswers = botAnswers;
     }
 
-    public void run() {
+    public void readPhrases() {
         try (BufferedReader reader = new BufferedReader(new FileReader(this.botAnswers))) {
             reader.lines()
-                    .map(x -> x.split("="))
-                    .forEach(x -> map.put(Integer.parseInt(x[0]), x[1]));
+                    .forEach(x -> list.add(x));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void writeLog(StringBuilder sb) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, StandardCharsets.UTF_8))) {
+            bw.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        readPhrases();
         Scanner scanner = new Scanner(System.in);
         String userSay = scanner.nextLine();
         boolean nonStop = true;
@@ -40,18 +49,14 @@ public class ConsoleChat {
                 nonStop = true;
             }
             if (nonStop) {
-                String answer = map.get((int) (Math.random() * map.size()));
+                String answer = list.get((int) (Math.random() * list.size()));
                 sb.append(answer + System.lineSeparator());
                 System.out.println(answer);
             }
             userSay = scanner.nextLine();
             sb.append(userSay + System.lineSeparator());
         }
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, StandardCharsets.UTF_8))) {
-            bw.write(sb.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        writeLog(sb);
     }
 
     public static void main(String[] args) {
